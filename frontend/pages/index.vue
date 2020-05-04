6190bd2d2999bb60e1cd9c5fa212e4346c3f8c25
Viktor Ilvovskyi <v.ilvovskyi@spd-ukraine.com>
2020-05-04 22:41:02 +0300

    Add mock chart using Chart.js

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="container-fluid">
-    <div class="h2">
-      Here will be user data
-    </div>
+    <canvas id="testChart" />
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
+
+export default {
+  mounted () {
+    const ctx = document.getElementById('testChart').getContext('2d')
+    // eslint-disable-next-line no-unused-vars
+    const testChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'],
+        datasets: [{
+          label: '# of Visits per Day',
+          data: [12, 19, 3, 5, 2, 3, 7],
+          backgroundColor: 'rgba(54, 162, 235, 1)'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        },
+        aspectRatio: 3
+      }
+    })
+  }
+}
 </script>
 
 <style>
