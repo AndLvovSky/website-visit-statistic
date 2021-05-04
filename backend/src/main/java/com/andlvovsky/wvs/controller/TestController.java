d7ef65fbae0a60986cf10d35401b49f41f9ebd00
andlvovsky <viktorilvovskyi@gmail.com>
2021-05-04 18:13:13 +0300

    Fix url to domain name

--- backend/src/main/java/com/andlvovsky/wvs/controller/TestController.java
+++ backend/src/main/java/com/andlvovsky/wvs/controller/TestController.java
@@ -12,6 +12,6 @@ import lombok.RequiredArgsConstructor;
 public class TestController {
   @GetMapping
   public String test() {
-    return "v5";
+    return "v6";
   }
 }
--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -1,4 +1,4 @@
 # Build static files
-NUXT_ENV_BACKEND_URL=http://wvs-lb-1523165624.us-east-1.elb.amazonaws.com npm run build
+NUXT_ENV_BACKEND_URL=https://wvs-backend.andlvovsky.com npm run build
 # Upload static files to S3
 aws s3 sync dist s3://website-visit-statistics --delete --acl public-read --cache-control no-cache
