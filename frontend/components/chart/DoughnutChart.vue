85cd87c02f965e5c8c4625971e49618a3d6fcc58
andlvovsky <viktorilvovskyi@gmail.com>
2021-05-04 15:47:18 +0300

    Autogenerate random colors if have many keys

--- frontend/components/chart/DoughnutChart.vue
+++ frontend/components/chart/DoughnutChart.vue
@@ -1,5 +1,6 @@
 <script>
 import { Doughnut } from 'vue-chartjs'
+import randomColor from 'randomcolor'
 
 export default {
   name: 'DoughnutChart',
@@ -24,16 +25,26 @@ export default {
   },
   methods: {
     getChartData () {
+      const colors = [
+        'rgba(255, 99, 71)',
+        'rgba(46, 112, 136)',
+        'rgba(46, 200, 136)',
+        'rgba(200, 244, 23)'
+      ]
+      const additionalCount = this.data.length - colors.length
+      if (additionalCount > 0) {
+        for (let i = 0; i < additionalCount; i++) {
+          colors.push(randomColor({
+            format: 'rgba',
+            alpha: 1
+          }))
+        }
+      }
       return {
         labels: this.data.map(item => item.label),
         datasets: [{
           data: this.data.map(item => item.value),
-          backgroundColor: [
-            'rgba(255, 99, 71)',
-            'rgba(46, 112, 136)',
-            'rgba(46, 200, 136)',
-            'rgba(200, 244, 23)'
-          ] // TODO: Support more colors
+          backgroundColor: colors
         }]
       }
     }
